94876ccfc2c10587e8c9faed69694d10d495ac4f
Ashu <ashutolosa3@gmail.com>
2020-12-11 16:38:13 +0300

    Creating Objects function Constructors

--- script.js
+++ script.js
@@ -10,14 +10,22 @@ var Person = function (name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function(){
-        console.log(2016 - this.yearOfBirth);
-    }
 }
+// Inheritance
+Person.prototype.calculateAge = function() {
+console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
 
 var john = new Person ('John', 1998, 'student');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
 
 john.calculateAge();
-
-var jane = new Person('Jane', 1969, 'designer');
 jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
